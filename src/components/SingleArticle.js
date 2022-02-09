bb4027b5238552bdf27ddfcccf244f5cc599f64c
MKD <kabir.dastgir@hotmail.com>
2022-02-09 10:38:52 +0000

    finish article page

--- src/components/SingleArticle.js
+++ src/components/SingleArticle.js
@@ -1,20 +1,31 @@
 import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import { useState } from 'react/cjs/react.development';
 import { getArticleById } from '../utils/api';
+import { shortDate } from '../utils/shortDate';
+import Comments from './Comments';
 
 const SingleArticle = () => {
+  const { article_id } = useParams();
   const [article, setArticle] = useState({});
 
   useEffect(() => {
-    getArticleById().then((res) => {
+    getArticleById(article_id).then((res) => {
       setArticle(res);
     });
-  });
+  }, []);
 
   return (
-    <div>
-      <h2>{article.title}</h2>
-    </div>
+    <>
+      <div>
+        <h3>Topic: {article.topic}</h3>
+        <h2>{article.title}</h2>
+        <p>{article.body}</p>
+        <a id="votes">Votes: {article.votes}</a>
+        <a id="date">Posted on: {shortDate(article.created_at)}</a>
+      </div>
+      <Comments article_id={article_id} />
+    </>
   );
 };
 
--- src/utils/api.js
+++ src/utils/api.js
@@ -21,3 +21,9 @@ export const getArticleById = (article_id) => {
     return res.data.article;
   });
 };
+
+export const getCommentsByArticleId = (article_id) => {
+  return api.get(`/articles/${article_id}/comments`).then((res) => {
+    return res.data.comments;
+  });
+};
