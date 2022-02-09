d7ecb3021339227373e67ba9f5bb5e3d8738c16e
MKD <kabir.dastgir@hotmail.com>
2022-02-09 10:38:00 +0000

    finish articles page

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { getArticles } from '../utils/api';
+import Topics from './Topics';
 
 const Articles = () => {
   const [articleList, setArticleList] = useState([]);
@@ -9,18 +10,20 @@ const Articles = () => {
     getArticles().then((res) => {
       setArticleList(res);
     });
-  });
+  }, []);
 
   return (
     <div>
+      <Topics />
       <ul>
         {articleList.map((article) => {
           return (
             <li key={article.article_id}>
               <Link to={`/articles/${article.article_id}`}>
                 <h2>{article.title}</h2>
-                <h3>{article.topic}</h3>
               </Link>
+              <h3>Topic: {article.topic}</h3>
+              <h3>Comments: {article.comment_count}</h3>
               <button>Votes: {article.votes}</button>
             </li>
           );
