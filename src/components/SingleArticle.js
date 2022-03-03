1397e12357480d317889f0df91257577df12a3a6
u1757390 <u1757390@unimail.hud.ac.uk>
2022-03-03 22:03:26 +0000

    update imports

--- src/components/SingleArticle.js
+++ src/components/SingleArticle.js
@@ -1,6 +1,5 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { useState } from "react/cjs/react.development";
 import { getArticleById } from "../utils/api";
 import { shortDate } from "../utils/shortDate";
 import { Comments } from "./index";
